e1f4cbbafd62d3d4dca491920a2e6e6434ed83d9
stgoneira <stgoneiran@gmail.com>
2023-04-06 18:45:49 -0400

    validación rut

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,3 +1,39 @@
+var Rut = {
+	// Valida el rut con su cadena completa "XXXXXXXX-X"
+	validaRut : function (rutCompleto) {
+		rutCompleto = rutCompleto.replaceAll(".","");
+		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+			return false;
+		var tmp 	= rutCompleto.split('-');
+		var digv	= tmp[1]; 
+		var rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+		
+		return (Rut.dv(rut) == digv );
+	},
+	dv : function(T){
+		var M=0,S=1;
+		for(;T;T=Math.floor(T/10))
+			S=(S+T%10*(9-M++%6))%11;
+		return S?S-1:'k';
+	}
+}
+
+const iRut = document.querySelector("input#rut");
+const mRut = document.querySelector("input#rut + span");
+iRut.addEventListener('blur', (e) => {
+    const rut = iRut.value;
+    if( !Rut.validaRut(rut) ) {
+        mRut.textContent = "RUT inválido!";
+        mRut.classList.add("error");
+        iRut.classList.add("error");
+    } else {
+        iRut.classList.remove("error");
+        mRut.classList.remove("error");
+        mRut.textContent = "";
+    }
+});
+
 const iNombre = document.querySelector("input#nombre");
 const mNombre = document.querySelector("input#nombre + span");
 iNombre.addEventListener('blur', (e) => {
@@ -9,5 +45,14 @@ iNombre.addEventListener('blur', (e) => {
         iNombre.classList.remove("error");
         mNombre.classList.remove("error");
         mNombre.textContent = "";
+    }
+});
+
+const sComuna = document.querySelector("select#comuna");
+const mComuna = document.querySelector("select#comuna + span");
+sComuna.addEventListener('blur', (e) => {
+    if( sComuna.value == "" ) {
+        mComuna.textContent = "Debe seleccionar una comuna";
+        mComuna.classList.add("error");
     }
 });
