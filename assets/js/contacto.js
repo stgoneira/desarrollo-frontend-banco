a030a7a2e2b6a2294efa6e8214c20253a45d08f9
stgoneira <stgoneiran@gmail.com>
2023-04-06 20:40:00 -0400

    ejemplo función JS para reducir línea de código para validación formulario

--- assets/js/contacto.js
+++ assets/js/contacto.js
@@ -1,3 +1,10 @@
+var Errores = {
+    nombre: {valido: false, mensaje: ""},
+    rut: {valido: false, mensaje: ""},
+    telefono: {valido: false, mensaje: ""}
+};
+
+
 var Rut = {
 	// Valida el rut con su cadena completa "XXXXXXXX-X"
 	validaRut : function (rutCompleto) {
@@ -33,7 +40,7 @@ iRut.addEventListener('blur', (e) => {
         mRut.textContent = "";
     }
 });
-
+/*
 const iNombre = document.querySelector("input#nombre");
 const mNombre = document.querySelector("input#nombre + span");
 iNombre.addEventListener('blur', (e) => {
@@ -47,6 +54,76 @@ iNombre.addEventListener('blur', (e) => {
         mNombre.textContent = "";
     }
 });
+*/
+
+// fnValidacion(vInput)
+// fnValidación -> devuelve true cuando el valor es válido 
+// fnValidación -> retorna un array [boolean, "mensaje..."]
+function validaCampo(vInput, vSpan, fnValidacion) {
+    vInput.addEventListener('blur', e => {
+        const resultado = fnValidacion(vInput);
+        if( ! resultado[0] ) {
+            vSpan.textContent = resultado[1];
+            vSpan.classList.add("error");
+            vInput.classList.add("error");
+        } else {
+            vSpan.textContent = "";
+            vSpan.classList.remove("error");
+            vInput.classList.remove("error");
+        }
+    });
+}
+
+validaCampo(
+    document.querySelector("#nombre"),
+    document.querySelector("#nombre + span"),
+    (vInput) => {        
+        const textoInput = vInput.value;        
+        if(textoInput.length < 3) {
+            return [false, 'Nombre muy corto'];
+        } else {
+            return [true, ''];
+        }
+    }
+);
+
+// template string
+const email = `
+Querido ${usuario.nombre},
+
+Lorem ipsum es de .... ${usuario.email} ....
+`;
+
+const fnValidacionTelefono = (vInput) => {        
+    const textoInput = vInput.value;        
+    if(textoInput.length < 3) {
+        return [false, 'Telefono muy corto'];
+    } else {
+        return [true, ''];
+    }
+};
+
+validaCampo(
+    document.querySelector("#telefono"),
+    document.querySelector("#telefono + span"),
+    fnValidacionTelefono
+);
+
+/*
+const iTelefono = document.querySelector("input#telefono");
+const mTelefono = document.querySelector("input#telefono + span");
+iTelefono.addEventListener('blur', (e) => {
+    if( iTelefono.value.length < 3 ) {
+        mTelefono.textContent = "Telefono muy corto";
+        mTelefono.classList.add("error");
+        iTelefono.classList.add("error");
+    } else {
+        iTelefono.classList.remove("error");
+        mTelefono.classList.remove("error");
+        mTelefono.textContent = "";
+    }
+});
+*/
 
 const sComuna = document.querySelector("select#comuna");
 const mComuna = document.querySelector("select#comuna + span");
@@ -55,4 +132,9 @@ sComuna.addEventListener('blur', (e) => {
         mComuna.textContent = "Debe seleccionar una comuna";
         mComuna.classList.add("error");
     }
+});
+
+const btnEnviar = document.querySelector("button[type='submit']");
+btnEnviar.addEventListener('submit', () => {
+    
 });
