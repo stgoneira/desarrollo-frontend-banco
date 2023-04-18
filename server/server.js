8317176d7b3b3e4176980acde733dd12ef52c829
stgoneira <stgoneiran@gmail.com>
2023-04-18 16:11:33 -0400

    quickstart fastify

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+};
+start();
