6c8a6850336d6888621cc29f7733e25fc9f1e9d3
stgoneira <stgoneiran@gmail.com>
2023-04-25 14:13:33 -0400

    change admin to admin-src

--- admin-src/next.config.js
+++ admin-src/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/administracion',  
+  basePath: '/admin',
   trailingSlash: true,
   images: {
     unoptimized: true
