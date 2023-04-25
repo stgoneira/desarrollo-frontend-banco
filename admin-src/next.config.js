b30316b9be044ef1fd7acf55c3d699aeb895e2d4
stgoneira <stgoneiran@gmail.com>
2023-04-25 16:36:12 -0400

    comentarios en NextJS config para que funcione "dev mode"

--- admin-src/next.config.js
+++ admin-src/next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /*
   output: 'export',
   basePath: '/admin',
   trailingSlash: true,
   images: {
     unoptimized: true
   }
+  */
 }
 
 module.exports = nextConfig
