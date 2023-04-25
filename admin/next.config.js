78dbc4127a9b9045165f4233bc0461cbe5246271
stgoneira <stgoneiran@gmail.com>
2023-04-25 13:19:30 -0400

    NextJS EsLint configuration, Standalone configuration

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  basePath: '/admin/out',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
