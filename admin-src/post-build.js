5ad1a265b1e94de0486be1d8e74b75b0474b1996
stgoneira <stgoneiran@gmail.com>
2023-05-04 13:47:26 -0400

    script mv folder

--- admin-src/post-build.js
+++ admin-src/post-build.js
@@ -1,15 +1,16 @@
 const fs = require('fs').promises;
 
 async function proceso() {
+    const distFolder = "../docs";
     try {    
-        await fs.rm('../admin', {force: true, recursive: true});
+        await fs.rm(distFolder, {force: true, recursive: true});
         console.log('Carpeta borrada exitosamente');
     } catch (err) {
         console.error('Error al borrar carpeta: ', err);
     }
 
     try {    
-        await fs.cp('out', '../admin', {force: true, recursive: true});
+        await fs.cp('out', distFolder, {force: true, recursive: true});
         console.log('Carpeta copiada exitosamente');
     } catch (err) {
         console.error('Error al copiar carpeta: ', err);
