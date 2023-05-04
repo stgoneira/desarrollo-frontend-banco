2f85845ca080d579ca73a534de685e09008c3f2a
stgoneira <stgoneiran@gmail.com>
2023-05-04 15:06:54 -0400

    delete docs

--- docs/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-function getBaseUrl() {
-    if( ['127.0.0.1', 'localhost'].includes(window.location.hostname) ) {
-        return 'http://localhost:3000';
-    } else {
-        return 'https://dapper-shorthaired-doom.glitch.me';
-    }
-}
